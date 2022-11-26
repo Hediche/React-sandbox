b918d37d1c7764a5fec8cf559050b064b175ff08
Khadijah Zunun <halqigularip@gmail.com>
2022-11-26 22:03:17 +0000

    Update App.js and styles.css

--- src/App.js
+++ src/App.js
@@ -3,9 +3,12 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>I am here again</h1>
-      <h2>What can I do to learn react faster now ?</h2>
-      <button>Click Here </button>
+      <h1>My favorite food</h1>
+      <ul>
+        <li>Lagman</li>
+        <li>Polo</li>
+        <li>Manta</li>
+      </ul>
     </div>
   );
 }
