964ff8606c8a4492c585885c936f53d73afd05f6
Khadijah Zunun <halqigularip@gmail.com>
2022-11-26 22:25:07 +0000

    Update App.js and styles.css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import "./styles.css";
 
+const fname = "Khadijah";
+const lname = "Zunun";
+const num = Math.floor(Math.random() * 10);
 export default function App() {
   return (
     <div className="App">
-      <h1>My favorite food</h1>
+      <h1>What would you like to eat, {fname + " " + lname} ? </h1>
+      <h2>
+        Nice, {name} ! you dont have to pay {num}$.{" "}
+      </h2>
       <ul>
-        <li>Lagman</li>
-        <li>Polo</li>
-        <li>Manta</li>
+        <li>لەغمەن</li>
+        <li>پولو</li>
+        <li>مانتا</li>
       </ul>
     </div>
   );
